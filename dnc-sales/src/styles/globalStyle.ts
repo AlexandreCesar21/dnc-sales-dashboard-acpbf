75ee6189ffaf858610e27f354be0d6dfe0463847
xandebarros9@gmail.com <127257670+AlexandreCesar21@users.noreply.github.com>
2025-03-01 19:38:49 -0300

    Criando estilo global

---
+++ dnc-sales/src/styles/globalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    body, html {
+        margin: 0;
+        padding: 0;
+        font-family: "Inter", serif;
+    }
+    h1, h2, p, ul, li {
+        margin: 0;
+        padding: 0;
+    }
+`
+
+
+
+
+
+
