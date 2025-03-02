04859e3706ccfe02a9ef24ba0798c6f5cb3effff
xandebarros9@gmail.com <127257670+AlexandreCesar21@users.noreply.github.com>
2025-03-02 10:46:36 -0300

    Feat: Create Themes

--- dnc-sales/src/main.tsx
+++ dnc-sales/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle } from './styles/globalStyle.ts'
+import { GlobalStyle, lighTheme } from './styles/'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle/>
-    <App />
+    <ThemeProvider theme={lighTheme}>  
+      <GlobalStyle/>
+      <App />
+    </ThemeProvider> 
   </StrictMode>
 )
