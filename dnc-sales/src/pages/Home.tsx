c80963ad6aff2cad24ce0d5831133a76a508bd92
xandebarros9@gmail.com <127257670+AlexandreCesar21@users.noreply.github.com>
2025-03-03 19:15:19 -0300

    Feat: Avatars List Component

--- dnc-sales/src/pages/Home.tsx
+++ dnc-sales/src/pages/Home.tsx
@@ -1,13 +1,33 @@
-import { CardComponent, Header } from "@/components"
+import { AvatarsList, CardComponent, Header } from "@/components"
 import { Container } from "@mui/material"
+import { currencyConverter } from "@/utils"
 
 function Home() {
-  
+    const mockListData = [
+      {
+        avatar: './dnc-avatar.svg',
+        name: 'Nome Sobrenome 1',
+        subtitle: currencyConverter(1234.54),
+      },
+      {
+        avatar: './dnc-avatar.svg',
+        name: 'Nome Sobrenome 2',
+        subtitle: currencyConverter(6444.26),
+      },
+      {
+        avatar: './dnc-avatar.svg',
+        name: 'Nome Sobrenome 3',
+        subtitle: currencyConverter(3873.98),
+      },
+    ]
     return (
       <>
         <Header/>
         <Container maxWidth="lg">
             <CardComponent>CARD</CardComponent>
+            <CardComponent>
+              <AvatarsList listData={mockListData}/>
+            </CardComponent>
         </Container>
         <h1>Home</h1>
       </>
